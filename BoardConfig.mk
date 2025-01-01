d60f4f4c80547be9290c59cf8bd723d927c4b072
dagobert <73406317+fuckdedeurkruk@users.noreply.github.com>
2025-01-01 18:42:36 +0100

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,12 +11,14 @@ KERNEL_PATH := $(DEVICE_PATH)-kernel
 include device/xiaomi/sm8650-common/BoardConfigCommon.mk
 
 # Display
-TARGET_SCREEN_DENSITY := 440
+TARGET_SCREEN_DENSITY := 480
 
 # Dtb/o
 BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := false
 
+TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 PRODUCT_COPY_FILES += \
@@ -34,9 +36,7 @@ BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE :=  $(KERNEL_PATH)/vendor_dlkm/module
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.1.57-android14-11-gd8b333a26dfd-ab11564698)
-    
-BOARD_SYSTEM_KERNEL_MODULES_LOAD := false
+    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.1.75-android14-11-gd57efa708101-ab12102151)
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := aurora
